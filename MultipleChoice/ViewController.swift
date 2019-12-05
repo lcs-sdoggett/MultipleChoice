4f3d973f2a0eb229446b10ea3b6b9bb4a41f2a5f
Scott Doggett <sdoggett@lcs.on.ca>
2019-12-04 22:19:08 -0500

    connections made

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var questionsAmount: UITextField!
+    @IBOutlet weak var studentAnswers: UITextField!
+    @IBOutlet weak var correctAnswers: UITextField!
+    @IBOutlet weak var outputTextView: UITextView!
+    
+    
+    // MARK: Methods
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: ACTIONS
 
-
+    @IBAction func checkAnswers(_ sender: Any) {
+    }
+    
 }
 
