80ec7b37dba21b1f79cc6ccd67109a326afbd9bb
Scott Doggett <sdoggett@lcs.on.ca>
2019-12-05 21:02:30 -0500

    comments added

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -19,16 +19,18 @@ class ViewController: UIViewController {
     
     // MARK: Methods
     
-    
+    // Runs when program opens
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionsAmount.becomeFirstResponder() // First text input is first responded
     }
     
     // MARK: ACTIONS
     
     @IBAction func checkAnswers(_ sender: Any) {
         
+        // Variable represents the amount of answers that are correct
         var numberOfCorrectAnswers = 0
         
         // Change amount of questions input into a string
@@ -37,48 +39,58 @@ class ViewController: UIViewController {
             return
         }
         
+        // Change sutdents answers into a string
         guard let studentAnswersAsString = studentAnswers.text, studentAnswersAsString.count != 0, studentAnswersAsString.count == questionsAmount else{
+            // Change output if the string can't be converted, or is not the right length
             outputTextView.text = "Please make sure you input exactly \(questionsAmount) student answers."
             return
         }
         
-        for character in studentAnswersAsString {
+        // Check if students answers are anything other then a,b,c,d or e
+        for character in studentAnswersAsString.lowercased() {
             switch character {
-            case  "A", "B", "C", "D", "E":
+            case  "a", "b", "c", "d", "e":
                 continue
             default:
+                // Change output if the string contains things other then a,b,c,d or e
                 outputTextView.text = "Student answers containsn invalid choices. Please ensure that only characters A, B, C, D or E are used."
                 return
             }
         }
 
-        
+        // Change correct answers into a string
         guard let correctAnswersAsString = correctAnswers.text, correctAnswersAsString.count != 0, correctAnswersAsString.count == questionsAmount else{
+            // Change output if the string can't be converted, or is not the right length
             outputTextView.text = "Please make sure you enter exactly \(questionsAmount) answers for the answer key."
             return
         }
         
-        for character in correctAnswersAsString {
+        // Check if correct answers are anything other then a,b,c,d or e
+        for character in correctAnswersAsString.lowercased() {
             switch character {
-            case  "A", "B", "C", "D", "E":
+            case  "a", "b", "c", "d", "e":
                 continue
             default:
+                // Change output if the string contains things other then a,b,c,d or e
                 outputTextView.text = "Correct answers containsn invalid choices. Please ensure that only characters A, B, C, D or E are used."
                 return
             }
         }
 
         
-        
+        // Cycle through each character in correctAnswers
         for (position, singleCharacter) in correctAnswersAsString.enumerated() {
             
+            // Get character from studentAnswers that has an index that corresponds to correctAnswers
             let index = studentAnswersAsString.index(studentAnswersAsString.startIndex, offsetBy: position)
             
+            // If they are the same, add 1 to right answers
             if singleCharacter == studentAnswersAsString[index] {
                 numberOfCorrectAnswers += 1
             }
         }
         
+        // Change output to display how many answers were right
         outputTextView.text = "The student answered \(numberOfCorrectAnswers ) questions(s) correctly."
     }
     
